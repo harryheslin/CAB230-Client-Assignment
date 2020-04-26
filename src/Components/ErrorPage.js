e86dc882baee25962b960d96df33870d7a3d57a6
Harry <n10438271@qut.edu.au>
2020-04-26 14:26:21 +1000

    rough search error handling implemented

--- src/Components/ErrorPage.js
+++ src/Components/ErrorPage.js
@@ -5,20 +5,19 @@ import {
 import "./ErrorPage.css";
 
 export default function ErrorPage(props) {
-  console.log(props);
   return (
-    <div className="main-div">
+    <div className="main-error-div">
       <div className="background-div">
         <h1>Uh oh</h1>
-        <h1>It looks like {props.message}</h1>
-        <Link to={"/"} style={{ textDecoration: "none" }}>
+        <h1>{props.error}</h1>
+        {/* <Link to={"/"} style={{ textDecoration: "none" }}>
           <div>
             <h1>
               <img className="logo" src="bulllogo.png" />
             </h1>
           </div>
           <h1 className="home">Home</h1>
-        </Link>
+        </Link> */}
       </div>
     </div>
   );
--- src/Components/StockInfo.js
+++ src/Components/StockInfo.js
@@ -1,32 +1,54 @@
 import React, { useEffect, useState } from 'react';
-
+import ErrorPage from './ErrorPage';
+import Search from './Search'
 
 export default function StockInfo(prop) {
 
     
     let { code } = prop
     const [companyData, setCompanyData] = useState([]);
+    const [error, setError] = useState("");
 
     useEffect(() => {
         fetch("http://131.181.190.87:3000/stocks/" + code)
-            .then(res => res.json())
-            .then(company =>
-                {
-                    return {
-                        timestamp: company.timestamp,
-                        symbol: company.symbol,
-                        name: company.name,
-                        industry: company.industry,
-                        open: company.open,
-                        high: company.high,
-                        low: company.low,
-                        close: company.close,
-                        volumes: company.volumes
-                    };
-                })
-            .then(data => setCompanyData(data));
+            .then((response) => {
+                if (response.status >= 200 && response.status <= 299) {
+                    return response.json();
+                } else if (response.status === 400) {
+                    setError("Invalid Query Parameters Used");
+                    throw Error();
+                }
+                else if (response.status === 404) {
+                    setError("No Entries For " + code +" Could Be Found");
+                    throw Error();
+                }
+            })
+            .then(company => {
+                return {
+                    timestamp: company.timestamp,
+                    symbol: company.symbol,
+                    name: company.name,
+                    industry: company.industry,
+                    open: company.open,
+                    high: company.high,
+                    low: company.low,
+                    close: company.close,
+                    volumes: company.volumes
+                };
+            })
+            .then(data => setCompanyData(data))
+            .catch(e => Error(e));
     }, [code]);
+
+    if (error === "") {
         return (
             <h1>{companyData.name}</h1>
         )
+    }
+    else {
+        return (
+            <ErrorPage error={error} />
+            
+        )
+    }
 }
